652b1a0aee05888e0001cdf6e18444eefde5d464
bt12121212 <teng198809182@gmail.com>
2023-07-21 16:22:31 +0800

    Initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container direction="vertical">
+    <el-input v-model="expression" placeholder="Enter your expression here..."></el-input>
+    <el-button type="primary" @click="calculate">Calculate</el-button>
+    <el-divider></el-divider>
+    <el-list>
+      <el-list-item v-for="(item, index) in history" :key="index">{{ item }}</el-list-item>
+    </el-list>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expression: '',
+      history: []
+    };
+  },
+  methods: {
+    calculate() {
+      try {
+        const result = eval(this.expression);
+        this.history.push(`${this.expression} = ${result}`);
+        this.expression = '';
+      } catch (error) {
+        console.log('Invalid expression');
+      }
+    }
+  }
+};
+</script>
